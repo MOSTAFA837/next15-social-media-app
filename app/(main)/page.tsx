ff208fd6cb93103e112063b1de425d7c1ce8ce34
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-08-07 01:57:52 +0200

    create post

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,12 @@
+import PostEditor from "@/components/post-editor";
 import React from "react";
 
 function Home() {
   return (
     <main className="h-[200vh] w-full bg-red-50">
-      <div className="w-full">Front page</div>
+      <div className="w-full">
+        <PostEditor />
+      </div>
     </main>
   );
 }
--- lib/validation.ts
+++ lib/validation.ts
@@ -1,18 +1,24 @@
-import {z} from "zod";
+import { z } from "zod";
 
-const requiredString = z.string().trim().min(1, "Required field")
+const requiredString = z.string().trim().min(1, "Required field");
 
 export const signUpSchema = z.object({
-    email: requiredString.email("Invalid email address"),
-    username: requiredString.regex(/^[a-zA-Z0-9_-]/, "Only letters, numbers, - and _ allawed"),
-    password: requiredString.min(8, "Password must be at least 8 characters")
-})
-
+  email: requiredString.email("Invalid email address"),
+  username: requiredString.regex(
+    /^[a-zA-Z0-9_-]/,
+    "Only letters, numbers, - and _ allawed"
+  ),
+  password: requiredString.min(8, "Password must be at least 8 characters"),
+});
 
 export const loginSchema = z.object({
-    username: requiredString,
-    password: requiredString,
-})
+  username: requiredString,
+  password: requiredString,
+});
 
-export type SignUpValues = z.infer<typeof signUpSchema>
-export type LoginValues = z.infer<typeof loginSchema>
+export const createPostSchema = z.object({
+  content: requiredString,
+});
+
+export type SignUpValues = z.infer<typeof signUpSchema>;
+export type LoginValues = z.infer<typeof loginSchema>;
