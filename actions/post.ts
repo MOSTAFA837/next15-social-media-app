adb625c991d79495ad4d206c4d3fb6048793b31d
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-08-16 10:29:24 +0200

    delete a post

--- actions/post.ts
+++ actions/post.ts
@@ -33,3 +33,22 @@ export async function getAllPosts() {
   revalidatePath("/");
   return posts;
 }
+
+export async function deletePost(id: string) {
+  const { user } = await validateRequest();
+  if (!user) throw new Error("Unauthorized");
+
+  const post = await prisma.post.findUnique({
+    where: { id },
+  });
+
+  if (!post) throw new Error("Post not found!");
+  if (post.userId !== user.id) throw new Error("Unauthorized");
+
+  const deletedPost = await prisma.post.delete({
+    where: { id },
+    include: postDataInclude,
+  });
+
+  return deletedPost;
+}
