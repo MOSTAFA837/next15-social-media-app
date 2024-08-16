e9c5b56864ef2c827b9dccb7a1b51fc518f0b827
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-08-16 16:56:36 +0200

    following feed

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,8 @@
 import PostEditor from "@/components/posts/editor/post-editor";
+import FollowingFeed from "@/components/posts/following-feed";
 import ForYouFeed from "@/components/posts/for-you-feed";
 import TrendsSidebar from "@/components/trends-sidebar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 async function Home() {
   return (
@@ -8,7 +10,18 @@ async function Home() {
       <div className="w-full min-w-0 space-y-5 flex-1">
         <PostEditor />
 
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
 
       <TrendsSidebar />
